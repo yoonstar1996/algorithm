cfd327bd5b25d4b4a1c559759456bf905e1148e8
Yoonstar <107985535+yoonstar1996@users.noreply.github.com>
2025-08-13 16:07:26 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -0,0 +1,10 @@
+function solution(chicken) {
+    var answer = 0;
+    
+    while(chicken >= 10) {
+        answer += Math.floor(chicken / 10);
+        chicken = Math.floor(chicken / 10) + chicken % 10;
+    }
+    
+    return answer;
+}
